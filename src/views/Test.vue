5be6fa214c14eaa1cb2b3093d25fd006abe6904e
cicada1208 <plumes@cycu.org.tw>
2021-02-10 09:54:34 +0800

    v-model

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,16 +1,45 @@
 <template>
   <div class="test">
     <h1>This is an test page</h1>
-    <span v-bind:title="title">
-      v-bind title test
-    </span>
-    <p v-if="true">v-if test</p>
-    <p v-if="true">v-for test</p>
-    <ol>
-      <li v-for="(item, index) in list" v-bind:key="index">{{ item.text }}</li>
-    </ol>
-    <button v-on:click="reverseTitle">v-on test</button>
-    <p>{{ titleReverse }}</p>
+    <table>
+      <tr>
+        <td>v-bind:</td>
+        <td>
+          <span v-bind:title="title">
+            hover this
+          </span>
+        </td>
+      </tr>
+      <tr>
+        <td>v-if:</td>
+        <td><span v-if="true">visible</span></td>
+      </tr>
+      <tr>
+        <td>v-for:</td>
+        <td>
+          <ol>
+            <li v-for="(item, index) in list" v-bind:key="index">
+              {{ item.text }}
+            </li>
+          </ol>
+        </td>
+      </tr>
+      <tr>
+        <td>v-on:</td>
+        <td>
+          <button v-on:click="reverseTitle">reverse title</button>
+          <p>{{ titleReverse }}</p>
+        </td>
+      </tr>
+      <tr>
+        <!-- 實現表單輸入和應用狀態之間的雙向綁定 -->
+        <td>v-model:</td>
+        <td>
+          <input v-model="title" />
+          <p>{{ title }}</p>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
