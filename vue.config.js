1b57eb6a5b469e0b3b5c51a432f70a0d9b2fbcfd
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-03-22 13:30:04 +0800

    vue.esm.js

--- vue.config.js
+++ vue.config.js
@@ -10,11 +10,26 @@ const pkgJson = require('./package.json');
 process.env.VUE_APP_VERSION = pkgJson.version;
 
 module.exports = {
+  // configureWebpack: 調整 webpack 配置，該對象將會被 webpack-merge 合併最終的 webpack 配置。
   configureWebpack: {
-    // Before you can debug your Vue components from VS Code
+    // source-map: Before you can debug your Vue components from VS Code
     // you need to update the generated webpack config to build
     // sourcemaps that contains more information for our debugger.
     devtool: 'source-map',
+    resolve: {
+      alias: {
+        // 若出現錯誤訊息:
+        // [Vue warn]: You are using the runtime-only build of Vue
+        // where the template compiler is not available.
+        // Either pre-compile the templates into render functions,
+        // or use the compiler-included build.
+        // 是因為若使用 const comp = { template: '<div>...</div>' }; 需引入帶 compiler 的 vue.esm.js。
+        // compiler: 可將模板字符串編譯成為 JavaScript 渲染函數的代碼。
+        // vue.runtime.esm.js: 只包含 runtime。
+        // vue.esm.js: 完整版包含 compiler and runtime。
+        vue$: 'vue/dist/vue.esm.js',
+      },
+    },
   },
   publicPath: process.env.NODE_ENV === 'production' ? `/${pkgJson.name}/` : '/',
 };
