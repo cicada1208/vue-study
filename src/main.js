c289d35ac98237b68f6464ae390fa43cdbe07757
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-03-28 23:58:05 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -7,6 +7,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router, // 通過 router 配置參數注入路由
-  store,
+  store, // 為了在 Vue 組件中訪問 this.$store property，需為 Vue 實例提供創建好的 store
   render: (h) => h(App),
 }).$mount('#app');
