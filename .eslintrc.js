aa50a3a8409959fb9bf6f9b3469aee07f42a1981
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-05-17 23:41:34 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,15 +6,17 @@ module.exports = {
     es6: true,
     node: true,
   },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
   extends: [
+    // add more generic rulesets here
     'eslint:recommended',
     'plugin:vue/essential',
     // 'plugin:prettier/recommended',
   ],
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
   rules: {
+    // override/add rules settings here
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'linebreak-style': ['off', 'windows'], // 關閉 win os 對換行的 lint
