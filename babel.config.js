e27dc994fd917729e5204b808aa9e65c4ac64d6c
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-05-22 17:00:25 +0800

    @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  // presets: 原使用 '@vue/cli-plugin-babel/preset'
-  // for Vue JSX 改使用 '@vue/babel-preset-jsx'
-  // 需 npm install @vue/babel-preset-jsx @vue/babel-helper-vue-jsx-merge-props
-  presets: ['@vue/babel-preset-jsx'],
+  // npm install @vue/babel-preset-jsx @vue/babel-helper-vue-jsx-merge-props
+  // presets: 原使用 '@vue/cli-plugin-babel/preset'，
+  // for Vue JSX 改使用 '@vue/babel-preset-jsx'。
+
+  // npm install -D @babel/core @babel/preset-env
+  // 該包確保只根據你指定的設置向應用程序中添加必要的pollyfills，
+  // 而不是手動安裝和導入所需的polyfills。
+  // presets: '@babel/preset-env'。
+  presets: ['@vue/babel-preset-jsx', '@babel/preset-env'],
 };
