e15e43038e6cadaaabc6cb61c98cffc41ade199f
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-05-23 15:38:10 +0800

    this.$vuetify.breakpoint

--- src/views/Vuetify.vue
+++ src/views/Vuetify.vue
@@ -30,6 +30,24 @@ export default {
     showValue(value, event) {
       alert(`tag name: ${event.target.tagName}, value: ${value}`);
     },
+    winResize() {
+      console.log('breakpoint name:', this.$vuetify.breakpoint.name);
+      console.log('breakpoint width:', this.$vuetify.breakpoint.width);
+      console.log('breakpoint height:', this.$vuetify.breakpoint.height);
+    },
+  },
+
+  mounted() {
+    this.winResize();
+    // passive: true 表示 listener 永遠不會調用 preventDefault()，
+    // 設定 true 的原因爲監聽事件時，頁面會有短暫的延遲來檢測是否要 preventDefault()，
+    // 關掉可避免 mobile 端卡頓。
+    window.addEventListener('resize', this.winResize, { passive: true });
+  },
+
+  beforeDestroy() {
+    if (typeof window === 'undefined') return;
+    window.removeEventListener('resize', this.winResize, { passive: true });
   },
 };
 </script>
