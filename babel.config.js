d05e95dbe31760518dd608fab232965208925866
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-05-25 23:55:05 +0800

    @babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -7,5 +7,15 @@ module.exports = {
   // 該包確保只根據你指定的設置向應用程序中添加必要的pollyfills，
   // 而不是手動安裝和導入所需的polyfills。
   // presets: '@babel/preset-env'。
-  presets: ['@vue/babel-preset-jsx', '@babel/preset-env'],
+  presets: [
+    '@vue/babel-preset-jsx',
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          esmodules: true,
+        },
+      },
+    ],
+  ],
 };
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,30 +1,41 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib/framework';
 
+// 7 Material Studies 可供選擇：
+// https://vuetifyjs.com/zh-Hans/features/presets/
+// 安裝 reply preset：vue add vuetify-preset-reply
+// import { preset } from 'vue-cli-plugin-vuetify-preset-reply/preset';
+
 Vue.use(Vuetify);
 
+// const options = {
+//   // theme: {
+//   //   dark: false,
+//   //   themes: {
+//   //     light: {
+//   //       primary: '#1976D2',
+//   //       secondary: '#424242',
+//   //       accent: '#82B1FF',
+//   //       error: '#FF5252',
+//   //       info: '#2196F3',
+//   //       success: '#4CAF50',
+//   //       warning: '#FB8C00',
+//   //     },
+//   //     dark: {
+//   //       primary: '#2196F3',
+//   //       secondary: '#424242',
+//   //       accent: '#FF4081',
+//   //       error: '#FF5252',
+//   //       info: '#2196F3',
+//   //       success: '#4CAF50',
+//   //       warning: '#FB8C00',
+//   //     },
+//   //   },
+//   // },
+// };
+
 export default new Vuetify({
-  // theme: {
-  //   dark: false,
-  //   themes: {
-  //     light: {
-  //       primary: '#1976D2',
-  //       secondary: '#424242',
-  //       accent: '#82B1FF',
-  //       error: '#FF5252',
-  //       info: '#2196F3',
-  //       success: '#4CAF50',
-  //       warning: '#FB8C00',
-  //     },
-  //     dark: {
-  //       primary: '#2196F3',
-  //       secondary: '#424242',
-  //       accent: '#FF4081',
-  //       error: '#FF5252',
-  //       info: '#2196F3',
-  //       success: '#4CAF50',
-  //       warning: '#FB8C00',
-  //     },
-  //   },
-  // },
+  // vuetify 會有自己的 defaults
+  // preset, // 再以 preset merge
+  // options, // 再以 user options merge
 });
