155bd6d7c3a5d672f938d5838e600e27c7875871
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-06-29 23:32:39 +0800

    aspect-ratio

--- src/components/VCardToolbarTest.vue
+++ src/components/VCardToolbarTest.vue
@@ -64,7 +64,7 @@
                 :src="card.src"
                 class="white--text align-end"
                 gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="140px"
+                :aspect-ratio="16 / 9"
               >
                 <v-card-title v-text="card.title"></v-card-title>
               </v-img>
