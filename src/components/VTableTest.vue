cdc0b263287314939e0bdaae0b71c0f01c5c46a7
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-08-02 12:03:56 +0800

    v-lazy

--- src/components/VTableTest.vue
+++ src/components/VTableTest.vue
@@ -1,19 +1,29 @@
 <template>
-  <v-container fluid>
-    <v-switch label="tableSelect" v-model="tableSelect" />
-    <v-data-table
-      item-key="id"
-      :show-select="tableSelect"
-      :headers="headers"
-      :items="items"
-    >
-    </v-data-table>
-  </v-container>
+  <!-- v-lazy: 默認情況下並不會顯示它的內容, 直到它與屏幕相交。 -->
+  <v-lazy
+    v-model="tableActive"
+    :options="{
+      threshold: 0.5,
+    }"
+    transition="fade-transition"
+  >
+    <v-container fluid>
+      <v-switch label="tableSelect" v-model="tableSelect" />
+      <v-data-table
+        item-key="id"
+        :show-select="tableSelect"
+        :headers="headers"
+        :items="items"
+      >
+      </v-data-table>
+    </v-container>
+  </v-lazy>
 </template>
 
 <script>
 export default {
   data: () => ({
+    tableActive: false,
     tableSelect: false,
     headers: [
       { text: '員工編號', value: 'id' },
