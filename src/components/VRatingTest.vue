105bec0df9f0da18d2073059a6c02a00f83ff56e
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-08-11 23:17:52 +0800

    v-rating

--- src/components/VRatingTest.vue
+++ src/components/VRatingTest.vue
@@ -1,20 +1,33 @@
 <template>
   <v-container fluid>
-    <v-rating
-      background-color="warning lighten-1"
-      color="warning"
-      half-increments
-      hover
-      length="10"
-      readonly
-      size="34"
-      :value="6.5"
-    ></v-rating>
+    <v-row>
+      <v-rating
+        v-model="rating"
+        length="10"
+        half-increments
+        hover
+        small
+        dense
+        color="warning"
+        background-color="warning lighten-1"
+        @input="ratingChange"
+      ></v-rating>
+      <span class="mt-1 grey--text text--lighten-1 text-caption">
+        ({{ rating }})
+      </span>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    rating: 6.5
+  }),
+  methods: {
+    ratingChange(num) {
+      console.log('rating:', num);
+    }
+  }
+};
 </script>
-
-<style></style>
