eda8eb50a1d6ed56cda5a7ea338cd68542fbe35e
cicada1208 <37774706+cicada1208@users.noreply.github.com>
2021-08-30 23:48:10 +0800

    v-timeline

--- src/components/VTimelineTest.vue
+++ src/components/VTimelineTest.vue
@@ -1,77 +1,108 @@
 <template>
   <v-container fluid>
-    <v-timeline align-top dense>
-      <v-timeline-item color="pink" small>
-        <v-row class="pt-1">
-          <v-col cols="3">
-            <strong>5pm</strong>
-          </v-col>
-          <v-col>
-            <strong>New Icon</strong>
-            <div class="text-caption">
-              Mobile App
-            </div>
-          </v-col>
-        </v-row>
-      </v-timeline-item>
-
-      <v-timeline-item color="teal lighten-3" small>
-        <v-row class="pt-1">
-          <v-col cols="3">
-            <strong>3-4pm</strong>
-          </v-col>
-          <v-col>
-            <strong>Design Stand Up</strong>
-            <div class="text-caption mb-2">
-              Hangouts
-            </div>
-            <v-avatar>
-              <v-img
-                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairFrida&accessoriesType=Kurt&hairColor=Red&facialHairType=BeardLight&facialHairColor=BrownDark&clotheType=GraphicShirt&clotheColor=Gray01&graphicType=Skull&eyeType=Wink&eyebrowType=RaisedExcitedNatural&mouthType=Disbelief&skinColor=Brown"
-              ></v-img>
-            </v-avatar>
-            <v-avatar>
-              <v-img
-                src="https://avataaars.io/?avatarStyle=Circle&topType=ShortHairFrizzle&accessoriesType=Prescription02&hairColor=Black&facialHairType=MoustacheMagnum&facialHairColor=BrownDark&clotheType=BlazerSweater&clotheColor=Black&eyeType=Default&eyebrowType=FlatNatural&mouthType=Default&skinColor=Tanned"
-              ></v-img>
-            </v-avatar>
-            <v-avatar>
-              <v-img
-                src="https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Sunglasses&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Pale"
-              ></v-img>
-            </v-avatar>
-          </v-col>
-        </v-row>
-      </v-timeline-item>
-
-      <v-timeline-item color="pink" small>
-        <v-row class="pt-1">
-          <v-col cols="3">
-            <strong>12pm</strong>
-          </v-col>
-          <v-col>
-            <strong>Lunch break</strong>
-          </v-col>
-        </v-row>
-      </v-timeline-item>
-
-      <v-timeline-item color="teal lighten-3" small>
-        <v-row class="pt-1">
-          <v-col cols="3">
-            <strong>9-11am</strong>
-          </v-col>
-          <v-col>
-            <strong>Finish Home Screen</strong>
-            <div class="text-caption">
-              Web App
-            </div>
-          </v-col>
-        </v-row>
-      </v-timeline-item>
-    </v-timeline>
+    <v-card class="mx-auto">
+      <v-card-title class="secondary white--text">
+        <span class="text-h6">Logs</span>
+        <v-spacer></v-spacer>
+        <v-btn
+          :outlined="interval == null"
+          :color="interval == null ? 'white' : 'basil'"
+          @click="interval == null ? start() : stop()"
+          dark
+        >
+          Realtime Logging
+        </v-btn>
+      </v-card-title>
+      <v-card-text class="py-0">
+        <v-timeline dense>
+          <v-slide-x-reverse-transition group hide-on-leave>
+            <v-timeline-item
+              v-for="item in items"
+              :key="item.id"
+              :color="item.color"
+              fill-dot
+              small
+            >
+              <v-alert :value="true" :color="item.color" :icon="item.icon" dark>
+                {{ item.text }}
+              </v-alert>
+            </v-timeline-item>
+          </v-slide-x-reverse-transition>
+        </v-timeline>
+      </v-card-text>
+    </v-card>
   </v-container>
 </template>
 
 <script>
-export default {};
+import moment from 'moment';
+
+const COLORS = ['error', 'success'];
+const ICONS = {
+  error: 'mdi-alert',
+  success: 'mdi-check-circle'
+};
+
+export default {
+  data: () => ({
+    interval: null,
+    items: [
+      {
+        id: 1,
+        color: 'success',
+        icon: ICONS.success,
+        text: moment().format('YYYY/MM/DD HH:mm:ss')
+      }
+    ],
+    id: 2
+  }),
+
+  beforeDestroy() {
+    this.stop();
+  },
+
+  methods: {
+    start() {
+      this.interval = setInterval(this.addLog, 3000);
+    },
+    stop() {
+      clearInterval(this.interval);
+      this.interval = null;
+    },
+    addLog() {
+      let { color, icon } = this.genLog();
+
+      const previousColor = this.items[0].color;
+
+      while (previousColor === color) {
+        color = this.genColor();
+        icon = this.genIcon(color);
+      }
+
+      this.items.unshift({
+        id: this.id++,
+        color,
+        icon,
+        text: moment().format('YYYY/MM/DD HH:mm:ss')
+      });
+
+      if (this.id > 5) {
+        this.items.pop();
+      }
+    },
+    genLog() {
+      const color = this.genColor();
+      return {
+        color,
+        icon: this.genIcon(color)
+      };
+    },
+    genColor() {
+      return COLORS[Math.floor(Math.random() * 2)];
+    },
+    genIcon(color) {
+      return ICONS[color];
+    }
+  }
+};
 </script>
